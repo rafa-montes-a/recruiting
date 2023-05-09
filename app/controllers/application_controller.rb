05378819ca3a301d407f881bb7d957deb4ee9d46
Rafael Montes <rmontes@chicagobooth.edu>
2023-05-09 01:18:04 +0000

    Menu ready

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  def index
+    render({ :template => "index_test.html.erb" })
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+get("/", { :controller => "application", :action => "index" })
+get("/index", { :controller => "application", :action => "index" })
+
 
 
 end
